e7c34a54860a941edb818425ab0d6554d0f824b6
gwimbrow <graham.wimbrow@gmail.com>
2014-01-20 14:36:13 -0800

    updates

--- loader.py
+++ loader.py
@@ -4,7 +4,6 @@ stdscr = curses.initscr()
 height,width = stdscr.getmaxyx()
 class room:
   def __init__(self):
-    self.door = ''
     self.current = ''
     self.firstrun = True
     self.specials = []
@@ -12,55 +11,53 @@ class room:
     self.reset()
   def reset(self):
     self.ypos,self.xpos = 0,0
-    self.script = []
-    self.special_list = []
+    self.current_specials = []
     self.current_selection = []
     self.current_visible = []
     self.count = 0
   def open_room(self,name):
-    if self.door != '':
-      del self.textarea
-      self.reset()
-    self.door = name.lower()
     if self.firstrun == True:
       with open('log') as master: self.current = master.readlines()[1].rstrip()
       self.firstrun = False
     else:
+      del self.textarea
+      self.reset()
       with open('transcript') as opened: transcript = opened.readlines()
       unopened = []
-      for names in os.listdir(sys.path[0]+'/house/'+self.door):
+      for names in os.listdir(sys.path[0]+'/house/'+name.lower()):
         if str(names+'\n') not in transcript: unopened.append(names)
       if len(unopened) != 0: self.current = random.choice(unopened)
       else:
-        self.door = 'epilogue'
+        name = 'epilogue'
         self.current = 'end'
-    with open('transcript','a') as opened: opened.write(self.current+'\n')
-    with open(sys.path[0]+'/house/'+self.door+'/'+self.current) as choice: self.script = choice.readlines()
-    th = len(self.script)+2
-    tw = max(len(self.current),len(max(self.script,key=len)))
+    script = []
+    with open(sys.path[0]+'/house/'+name.lower()+'/'+self.current) as choice: script = choice.readlines()
+    th = len(script)+2
+    tw = max(len(self.current),len(max(script,key=len)))
     self.textarea = curses.newpad(th,tw)
     y,x = 0,0
-    for i in range(len(self.script)+1):
-      if i < len(self.script): words = self.script[i].rstrip().split(' ')
+    for i in range(len(script)+1):
+      if i < len(script): words = script[i].rstrip().split(' ')
       else: words = self.current.rstrip().split(' ')
       for j in words:
         if j.lower() in self.specials:
           self.textarea.addstr(y,x,j,curses.color_pair(1))
-          self.special_list.append([y,x,j])
+          self.current_specials.append([y,x,j])
         else: self.textarea.addstr(y,x,j)
         x += len(j)+1
       y += 1
       x = 0
-    with open('log','w') as master: master.write(self.door+'\n'+self.current)
+    with open('transcript','a') as opened: opened.write(self.current+'\n')
+    with open('log','w') as master: master.write(name.lower()+'\n'+self.current)
   def nextspecial(self):
     global height,width
     visible = []
-    for j in self.special_list:
+    for j in self.current_specials:
       if self.ypos-1 < j[0] < self.ypos+height and self.xpos-1 < j[1] < self.xpos+width: visible.append(j)
     if len(visible) != 0:
       if self.current_visible != visible: self.count = 0
       self.textarea.chgat(visible[self.count][0],visible[self.count][1],len(visible[self.count][2]),curses.color_pair(2))
-      if self.current_selection != []:
+      if self.current_selection != []: 
         self.textarea.chgat(self.current_selection[0],self.current_selection[1],len(self.current_selection[2]),curses.color_pair(1))
       self.current_visible = visible
       self.current_selection = visible[self.count]
