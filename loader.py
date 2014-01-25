b92932c35aca807579d3a491810b7706a70db31a
gwimbrow <graham.wimbrow@gmail.com>
2014-01-25 12:08:08 -0800

    introduced tracking using time-stamped files

--- loader.py
+++ loader.py
@@ -1,67 +1,67 @@
 #! /usr/bin/env python
-import sys, os, random, curses
-stdscr = curses.initscr()
-height,width = stdscr.getmaxyx()
+import sys, os, curses
+stdscr=curses.initscr()
+height,width=stdscr.getmaxyx()
+firstrun=True
 class room:
   def __init__(self):
-    self.current = ''
-    self.firstrun = True
-    self.specials = []
-    for names in os.listdir(sys.path[0]+'/house/'): self.specials.append(names)
+    self.storyhash={}
+    with open('log') as log: pairs=log.readlines()[1].split(',')
+    for p in pairs:
+      key,value=p.split(':')[0],p.split(':')[1]
+      self.storyhash[key]=int(value)
     self.reset()
   def reset(self):
-    self.ypos,self.xpos = 0,0
-    self.current_specials = []
-    self.current_selection = []
-    self.current_visible = []
-    self.count = 0
-  def open_room(self,name):
-    if self.firstrun == True:
-      with open('log') as master: self.current = master.readlines()[1].rstrip()
-      self.firstrun = False
-    else:
+    self.ypos,self.xpos=0,0
+    self.current_specials=[]
+    self.current_selection=[]
+    self.current_visible=[]
+    self.count=0
+  def open_room(self,sub):
+    global firstrun
+    if firstrun==False:
       del self.textarea
       self.reset()
-      with open('transcript') as opened: transcript = opened.readlines()
-      unopened = []
-      for names in os.listdir(sys.path[0]+'/house/'+name.lower()):
-        if str(names+'\n') not in transcript: unopened.append(names)
-      if len(unopened) != 0: self.current = random.choice(unopened)
-      else:
-        name = 'epilogue'
-        self.current = 'end'
-    script = []
-    with open(sys.path[0]+'/house/'+name.lower()+'/'+self.current) as choice: script = choice.readlines()
-    th = len(script)+2
-    tw = max(len(self.current),len(max(script,key=len)))
-    self.textarea = curses.newpad(th,tw)
-    y,x = 0,0
-    for i in range(len(script)+1):
-      if i < len(script): words = script[i].rstrip().split(' ')
-      else: words = self.current.rstrip().split(' ')
-      for j in words:
-        if j.lower() in self.specials:
-          self.textarea.addstr(y,x,j,curses.color_pair(1))
-          self.current_specials.append([y,x,j])
-        else: self.textarea.addstr(y,x,j)
-        x += len(j)+1
-      y += 1
-      x = 0
-    with open('transcript','a') as opened: opened.write(self.current+'\n')
-    with open('log','w') as master: master.write(name.lower()+'\n'+self.current)
+    script=[]
+    with open(sys.path[0]+'/data/'+sub+'/'+str(self.storyhash[sub])) as choice: script=choice.readlines()
+    th=len(script)
+    tw=len(max(script,key=len))
+    self.textarea=curses.newpad(th+1,tw+1)
+    y,x=0,0
+    for i in range(len(script)):
+      words=script[i].rstrip().split(' ')
+      for w in words:
+        if w.lower() in self.storyhash.keys():
+          self.textarea.addstr(y,x,w,curses.color_pair(1))
+          self.current_specials.append([y,x,w])
+        else: self.textarea.addstr(y,x,w)
+        x+=len(w)+1
+      y+=1
+      x=0
+    files=[]
+    for names in os.listdir(sys.path[0]+'/data/'+sub):
+      files.append(int(names))
+    if self.storyhash[sub]!=max(files):
+      files.sort()
+      self.storyhash[sub]=files[files.index(self.storyhash[sub])+1]
+    with open('log','w') as log: 
+      storystring = []
+      for n in range(len(self.storyhash.keys())): storystring.append(self.storyhash.keys()[n]+':'+str(self.storyhash.values()[n]))
+      log.write(sub.lower()+'\n'+','.join(storystring))
+    if firstrun==True: firstrun=False
   def nextspecial(self):
     global height,width
-    visible = []
+    visible=[]
     for j in self.current_specials:
-      if self.ypos-1 < j[0] < self.ypos+height and self.xpos-1 < j[1] < self.xpos+width: visible.append(j)
-    if len(visible) != 0:
-      if self.current_visible != visible: self.count = 0
+      if self.ypos-1<j[0]<self.ypos+height and self.xpos-1<j[1]<self.xpos+width: visible.append(j)
+    if len(visible)!=0:
+      if self.current_visible!=visible: self.count = 0
       self.textarea.chgat(visible[self.count][0],visible[self.count][1],len(visible[self.count][2]),curses.color_pair(2))
-      if self.current_selection != []: 
+      if self.current_selection!=[]: 
         self.textarea.chgat(self.current_selection[0],self.current_selection[1],len(self.current_selection[2]),curses.color_pair(1))
-      self.current_visible = visible
-      self.current_selection = visible[self.count]
-      if self.count < len(visible)-1: self.count += 1
+      self.current_visible=visible
+      self.current_selection=visible[self.count]
+      if self.count<len(visible)-1: self.count+=1
       else: self.count = 0
       return self.current_selection[2]
     else: return ''
@@ -73,18 +73,18 @@ def main(stdscr):
   curses.init_color(2,1000,0,0)
   curses.init_pair(1,1,curses.COLOR_BLACK);
   curses.init_pair(2,2,curses.COLOR_BLACK);
-  with open('log') as master: r.open_room(master.readlines()[0].rstrip())
-  lock = ''
+  with open('log') as log: r.open_room(log.readlines()[0].rstrip())
+  lock=''
   while True:
     r.textarea.refresh(r.ypos,r.xpos,max(0,(height/2)-(r.textarea.getmaxyx()[0]/2)),max(0,(width/2)-(r.textarea.getmaxyx()[1]/2)),min(height-1,(height/2)+(r.textarea.getmaxyx()[0]/2)),min(width-1,(width/2)+(r.textarea.getmaxyx()[1]/2)))
-    k = r.textarea.getch()
-    if k == ord('q'): break
-    elif k == ord('\t'): lock = r.nextspecial()
-    elif k == ord('e') and lock != '': r.open_room(lock)
-    elif k == ord('s') and r.ypos+height < r.textarea.getmaxyx()[0]: r.ypos += 1
-    elif k == ord('w') and 0 < r.ypos: r.ypos -= 1
-    elif k == ord('d') and r.xpos+width < r.textarea.getmaxyx()[1]: r.xpos += 1
-    elif k == ord('a') and 0 < r.xpos: r.xpos -= 1
+    k=r.textarea.getch()
+    if k==ord('q'): break
+    elif k==ord('\t'): lock=r.nextspecial()
+    elif k==ord('e') and lock!='': r.open_room(lock)
+    elif k==ord('s') and r.ypos+height<r.textarea.getmaxyx()[0]: r.ypos+=1
+    elif k==ord('w') and 0<r.ypos: r.ypos-=1
+    elif k==ord('d') and r.xpos+width<r.textarea.getmaxyx()[1]: r.xpos+=1
+    elif k==ord('a') and 0<r.xpos: r.xpos-=1
   stdscr.clear()
   stdscr.refresh()
   curses.curs_set(1)
