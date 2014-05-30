0253182da8adc4a44d08910fb0c284852298dce6
gwimbrow <graham.wimbrow@gmail.com>
2014-05-30 09:24:20 -0700

    real changes

--- tektere.py
+++ tektere.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import sys,os,re,curses
+import sys,os,re,curses,string
 stdscr=curses.initscr()
 height,width=stdscr.getmaxyx()
 class message:
@@ -15,10 +15,17 @@ class message:
     self.count=0
     self.links=[]
     self.selection=[]
-  def load(self,address,source=''):
-    if source=='': script=[':'.join([address,files]) for files in os.listdir('/'.join([sys.path[0],'data',address]))]
+  def load(self,verse):
+    if verse.endswith(':'):
+      script=sorted([''.join([verse,files]) for files in os.listdir('/'.join([sys.path[0],'data',verse.replace(':','')]))])
+      c=map(int,verse.replace(':','').split('.'))
+      self.occupied=[names+':' for names in os.listdir('/'.join([sys.path[0],'data']))]
+      self.adjacents=['.'.join(map(str,[c[0]-1,c[1]]))+':','.'.join(map(str,[c[0],c[1]+1]))+':','.'.join(map(str,[c[0]+1,c[1]]))+':','.'.join(map(str,[c[0],c[1]-1]))+':']
     else:
-      with open('/'.join([sys.path[0],'data',address,source])) as choice: script=choice.readlines()
+      address=verse.split(':')
+      with open('/'.join([sys.path[0],'data',address[0],address[1]])) as choice: script=choice.readlines()
+      self.occupied=sorted([':'.join([address[0],files]) for files in os.listdir('/'.join([sys.path[0],'data',address[0]]))])
+      self.adjacents=[self.occupied[max(0,self.occupied.index(verse)-1)],' ',self.occupied[min(len(self.occupied)-1,self.occupied.index(verse)+1)],' ']
     self.reset(len(script)+1,len(max(script,key=len))+1)
     y=0
     for s in script:
@@ -39,7 +46,7 @@ class message:
             else: self.textarea.addch(y,x,c,curses.color_pair(1))
             x+=1
       y+=1
-    with open('log','w') as log: log.write(address+':'+source)
+    with open('log','w') as log: log.write(verse)
   def navigate(self):
     self.textarea.chgat(self.links[self.count][0],self.links[self.count][1],len(self.links[self.count][2]),curses.color_pair(3))
     if self.selection!=[] and self.selection!=self.links[self.count]: self.textarea.chgat(self.selection[0],self.selection[1],len(self.selection[2]),curses.color_pair(2))
@@ -61,8 +68,7 @@ def main(stdscr):
   curses.init_pair(4,5,1) # quote / box drawing
   stdscr.bkgd(' ',curses.color_pair(1))
   curses.curs_set(0)
-  with open('log') as log: save=log.read().rstrip().split(':')
-  m.load(save[0],save[1])
+  with open('log') as log: m.load(log.read().rstrip())
   target=''
   while True:
     m.textarea.refresh(m.ypos,0,max(0,(height/2)-(m.textarea.getmaxyx()[0]/2)),(width/2)-(m.textarea.getmaxyx()[1]/2),min(height-2,(height/2)+(m.textarea.getmaxyx()[0]/2)),(width/2)+(m.textarea.getmaxyx()[1]/2))
@@ -70,10 +76,13 @@ def main(stdscr):
     if k==ord('q'): break
     elif k==ord('\t') and len(m.links)>0: target=m.navigate()
     elif k==ord('e') and target!='':
-      save=target.split(':')
-      m.load(save[0],save[1])
+      m.load(target)
       target=''
     elif k==ord('s') and m.ypos+height<m.textarea.getmaxyx()[0]: m.ypos+=1
     elif k==ord('w') and 0<m.ypos: m.ypos-=1
+    elif k==ord('i') and m.adjacents[0] in m.occupied: m.load(m.adjacents[0])
+    elif k==ord('l') and m.adjacents[1] in m.occupied: m.load(m.adjacents[1])
+    elif k==ord('k') and m.adjacents[2] in m.occupied: m.load(m.adjacents[2])
+    elif k==ord('j') and m.adjacents[3] in m.occupied: m.load(m.adjacents[3])
   curses.curs_set(1)
 curses.wrapper(main)
