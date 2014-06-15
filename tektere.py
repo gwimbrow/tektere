1e387d631754d239a006a294947d37cd9dd7168a
gwimbrow <graham.wimbrow@gmail.com>
2014-06-15 16:54:02 -0700

    added write mode

--- tektere.py
+++ tektere.py
@@ -1,13 +1,23 @@
+# To do: create an additional folder subdirectory below the /data dir that will contain the folders for different scenarios.
+# When a scenario is loaded, reset the dict stored in the log file to {}.
+# Load a config file from the scenario folder which contains color information and a dictionary sequence to compare with the dict in the log file.
+# Use if len(set(self.trail.items()) & set(self.scenario.items()))==len(scenario.items()):
+# to check if the dicts have all the same values and same number of values. if true, then load the next scenario.
+# The scenario folders will be numbered sequentially. Display the scenario number at the top-left corner of the screen (above the UARROW)
+#
 #! /usr/bin/env python
-import os,re,curses,curses.textpad
+import sys,os,re,ast,getopt,curses
 from subprocess import call
 stdscr=curses.initscr()
 height,width=stdscr.getmaxyx()
 class message:
   def __init__(self):
-    self.keys=[ 'w: up / s: down / tab: select / e: enter / r: revise / q: quit','i: north, previous / l: east / k: south, next / j: west',
-    ['first','second','third','fourth','fifth','sixth','seventh','eigth','ninth']]
-  def reset(self,h,w,v):
+    self.write=False
+    options,rmd=getopt.getopt(sys.argv[1:],'w')
+    for opt,arg in options:
+      if opt=='-w': self.write=True
+    self.keys=[ 'w: up / s: down / tab: select / e: enter / q: quit',' / r: revise','i: north, previous / l: east / k: south, next / j: west',['I','II','III','IV','V']]
+  def reset(self,h,w):
     os.system('setterm -cursor off')
     self.h=h
     self.w=w
@@ -18,52 +28,55 @@ class message:
     if 'textarea' in locals(): del self.textarea
     stdscr.clear()
     stdscr.addstr(height-1,0,self.keys[0])
-    stdscr.addstr(height-1,width-len(self.keys[1])-1,self.keys[1])
-    if v.endswith(':'):
-      c=map(int,v[:v.index(':')].split('.'))
+    if self.write==True: stdscr.addstr(height-1,len(self.keys[0])+1,self.keys[1])
+    stdscr.addstr(height-1,width-len(self.keys[2])-1,self.keys[2])
+    self.textarea=curses.newpad(self.h,self.w)
+    self.textarea.bkgd(' ',curses.color_pair(4))
+  def load(self,verse):
+    with open('log') as log: self.trail=ast.literal_eval(log.readlines()[1].rstrip())
+    if verse.endswith(':'):
+      with open('/'.join(['data',verse[:verse.index(':')],'scene'])) as choice: script=choice.readlines()
+      script.extend('\n')
+      script.extend(sorted([''.join(['Act ',verse,files]) for files in os.listdir('/'.join(['data',verse[:verse.index(':')]])) if files!='scene']))
+      script.extend('\n')
+      n=[verse+f for f in self.keys[3] if ''.join(['Act ',verse,f]) not in script]
+      if self.write==True: script.append('write '+n[0])
+      self.reset(len(script)+7,max(len(verse),len(max(script,key=len)))+3)
+      c=map(int,verse[:verse.index(':')].split('.'))
       self.occupied=[names+':' for names in os.listdir('data')]
       self.adjacents=['.'.join(map(str,[max(0,c[0]-1),c[1]]))+':','.'.join(map(str,[c[0],c[1]+1]))+':','.'.join(map(str,[c[0]+1,c[1]]))+':','.'.join(map(str,[c[0],max(0,c[1]-1)]))+':']
-      if v[0]!='0':
-        if m.adjacents[0] in m.occupied: stdscr.addch(2,width/2,curses.ACS_UARROW)
-        else: stdscr.addch(2,width/2,curses.ACS_UARROW,curses.color_pair(3))
-      if v[2]!='9':
-        if m.adjacents[1] in m.occupied: stdscr.addch(height/2,width-2,curses.ACS_RARROW)
-        else: stdscr.addch(height/2,width-2,curses.ACS_RARROW,curses.color_pair(3))
-      if v[0]!='9':
-        if m.adjacents[2] in m.occupied: stdscr.addch(height-2,width/2,curses.ACS_DARROW)
-        else: stdscr.addch(height-2,width/2,curses.ACS_DARROW,curses.color_pair(3))
-      if v[2]!='0':
-        if m.adjacents[3] in m.occupied: stdscr.addch(height/2,2,curses.ACS_LARROW)
-        else: stdscr.addch(height/2,2,curses.ACS_LARROW,curses.color_pair(3))
+      if verse[0]!='0':
+        if verse in self.trail.keys() and self.trail[verse]==0: stdscr.addch(2,width/2,curses.ACS_UARROW)
+        elif self.adjacents[0] in self.occupied: stdscr.addch(2,width/2,curses.ACS_UARROW,curses.color_pair(3))
+        elif self.write==True: stdscr.addch(2,width/2,curses.ACS_UARROW,curses.color_pair(2))
+      if verse[2]!=min(9,len(self.occupied)-1):
+        if verse in self.trail.keys() and self.trail[verse]==1: stdscr.addch(height/2,width-2,curses.ACS_RARROW)
+        elif self.adjacents[1] in self.occupied: stdscr.addch(height/2,width-2,curses.ACS_RARROW,curses.color_pair(3))
+        elif self.write==True: stdscr.addch(height/2,width-2,curses.ACS_RARROW,curses.color_pair(2))
+      if verse[0]!=min(9,len(self.occupied)-1):
+        if verse in self.trail.keys() and self.trail[verse]==2: stdscr.addch(height-2,width/2,curses.ACS_DARROW)
+        elif self.adjacents[2] in self.occupied: stdscr.addch(height-2,width/2,curses.ACS_DARROW,curses.color_pair(3))
+        elif self.write==True: stdscr.addch(height-2,width/2,curses.ACS_DARROW,curses.color_pair(2))
+      if verse[2]!='0':
+        if verse in self.trail.keys() and self.trail[verse]==3: stdscr.addch(height/2,2,curses.ACS_LARROW)
+        elif self.adjacents[3] in self.occupied: stdscr.addch(height/2,2,curses.ACS_LARROW,curses.color_pair(3))
+        elif self.write==True: stdscr.addch(height/2,2,curses.ACS_LARROW,curses.color_pair(3))
     else:
-      self.occupied=sorted([':'.join([v[:v.index(':')],files]) for files in os.listdir('/'.join(['data',v[:v.index(':')]])) if files!='scene'])
-      if self.occupied.index(v)==0: prev=' '
-      else: prev=self.occupied[self.occupied.index(v)-1]
-      try: adv=self.occupied[self.occupied.index(v)+1]
+      with open('/'.join(['data',verse[:verse.index(':')],verse[verse.index(':')+1:]])) as choice: script=choice.readlines()
+      self.reset(len(script)+7,max(len(verse),len(max(script,key=len)))+3)
+      self.occupied=sorted([':'.join([verse[:verse.index(':')],files]) for files in os.listdir('/'.join(['data',verse[:verse.index(':')]])) if files!='scene'])
+      if self.occupied.index(verse)==0: prev=' '
+      else: prev=self.occupied[self.occupied.index(verse)-1]
+      try: adv=self.occupied[self.occupied.index(verse)+1]
       except IndexError: adv=' '
       self.adjacents=[prev,' ',adv,' ']
       if m.adjacents[0] in m.occupied: stdscr.addch(2,width/2,curses.ACS_UARROW)
       if m.adjacents[2] in m.occupied: stdscr.addch(height-2,width/2,curses.ACS_DARROW)
     stdscr.refresh()
-    self.textarea=curses.newpad(self.h,self.w)
-    self.textarea.bkgd(' ',curses.color_pair(4))
-    self.textarea.addstr(1,1,v[:v.index(':')+1],curses.color_pair(2))
-    self.links.append([1,1,v[:v.index(':')+1]])
-    self.textarea.addstr(1,6,v[v.index(':')+1:],curses.color_pair(1))
-    self.textarea.addch(2,0,curses.ACS_LTEE)
-    self.textarea.addch(2,self.w-1,curses.ACS_RTEE)
-    self.textarea.hline(2,1,curses.ACS_HLINE,self.w-2)
-  def load(self,verse):
-    if verse.endswith(':'):
-      with open('/'.join(['data',verse[:verse.index(':')],'scene'])) as choice: script=choice.readlines()
-      script.extend('\n')
-      script.extend(sorted([''.join(['Act the ',verse,files]) for files in os.listdir('/'.join(['data',verse[:verse.index(':')]])) if files!='scene']))
-      script.extend('\n')
-      n=[verse+f for f in self.keys[2] if ''.join(['Act the ',verse,f]) not in script]
-      script.append(' '.join(['write of the',n[0],'encounter']))
-    else:
-      with open('/'.join(['data',verse[:verse.index(':')],verse[verse.index(':')+1:]])) as choice: script=choice.readlines()
-    self.reset(len(script)+7,max(len(verse),len(max(script,key=len)))+3,verse)
+    self.textarea.addstr(1,1,verse[:verse.index(':')+1],curses.color_pair(2))
+    self.links.append([1,1,verse[:verse.index(':')+1]])
+    self.textarea.addstr(1,6,verse[verse.index(':')+1:],curses.color_pair(1))
+    self.textarea.hline(2,0,curses.ACS_HLINE,self.w-1)
     y=4
     for s in script:
       x=1
@@ -81,22 +94,10 @@ class message:
             if verse.endswith(':') and not w.endswith(':'): w=w[w.index(':')+1:]
             self.textarea.addstr(y,x,w,curses.color_pair(2))
             self.links.append([y,x,w])
-            x+=len(w)
-          else:
-            for c in w:
-              if c=='@': self.textarea.addch(y,x,curses.ACS_ULCORNER)
-              elif c=='#': self.textarea.addch(y,x,curses.ACS_URCORNER)
-              elif c=='$': self.textarea.addch(y,x,curses.ACS_LLCORNER)
-              elif c=='%': self.textarea.addch(y,x,curses.ACS_LRCORNER)
-              elif c=='|': self.textarea.addch(y,x,curses.ACS_VLINE)
-              elif c=='_': self.textarea.addch(y,x,curses.ACS_HLINE)
-              else: self.textarea.addch(y,x,c,curses.color_pair(1))
-              x+=1
+          else: self.textarea.addstr(y,x,w,curses.color_pair(1))
+          x+=len(w)
         y+=1
-    self.textarea.addch(y+1,0,curses.ACS_LTEE)
-    self.textarea.addch(y+1,self.w-1,curses.ACS_RTEE)
-    self.textarea.hline(y+1,1,curses.ACS_HLINE,self.w-2)
-    with open('log','w') as log: log.write(verse)
+    with open('log','w') as log: log.write('\n'.join([verse,str(self.trail)]))
   def navigate(self):
     self.textarea.chgat(self.links[self.count][0],self.links[self.count][1],len(self.links[self.count][2]),curses.color_pair(3))
     if self.selection!=[] and self.selection!=self.links[self.count]: self.textarea.chgat(self.selection[0],self.selection[1],len(self.selection[2]),curses.color_pair(2))
@@ -118,7 +119,6 @@ class message:
       self.load(self.textarea.instr(1,1,4)+l)
 m = message()
 def main(stdscr):
-  global height,width
   curses.init_color(1,1000,1000,1000)
   curses.init_pair(1,1,0) # base
   curses.init_color(3,1000,106,255)
@@ -128,7 +128,7 @@ def main(stdscr):
   curses.init_color(5,275,39,1000)
   curses.init_pair(4,5,0) # box drawing
   stdscr.bkgd(' ',curses.color_pair(4))
-  with open('log') as log: m.load(log.read().rstrip())
+  with open('log') as log: m.load(log.readlines()[0].rstrip())
   target=''
   kc=[ord('i'),ord('l'),ord('k'),ord('j')]
   while True:
@@ -139,10 +139,11 @@ def main(stdscr):
     elif k==ord('e') and target!='':
       if ':' not in target:
         try: m.load(m.textarea.instr(1,1,4)+target)
-        except IOError: m.revise(target)
+        except IOError:
+          if m.write==True: m.revise(target)
       else: m.load(target)
       target=''
-    elif k==ord('r') and target!='':
+    elif k==ord('r') and m.write==True and target!='':
       if target.endswith(':'): m.revise('scene')
       elif ':' not in target:
         m.revise(target)
@@ -150,7 +151,10 @@ def main(stdscr):
     elif k==ord('s') and m.ypos+height-4<m.h: m.ypos+=1
     elif k==ord('w') and 0<m.ypos: m.ypos-=1
     elif k in kc:
+      if m.adjacents[kc.index(k)].endswith(':'):
+        m.trail[m.textarea.instr(1,1,4)]=int(kc.index(k))
+        with open('log','w') as log: log.write('\n'.join([m.textarea.instr(1,1,4),str(m.trail)]))
+        if m.write==True and m.adjacents[kc.index(k)] not in m.occupied: m.revise(m.adjacents[kc.index(k)])
       if m.adjacents[kc.index(k)] in m.occupied: m.load(m.adjacents[kc.index(k)])
-      elif m.adjacents[kc.index(k)].endswith(':'): m.revise(m.adjacents[kc.index(k)])
   os.system('setterm -cursor on')
 curses.wrapper(main)
