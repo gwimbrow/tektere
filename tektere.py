2fdd8e31fca96521a75461ede004f567db0cb456
gwimbrow <graham.wimbrow@gmail.com>
2014-11-03 08:23:43 -0800

    dreaming

--- tektere.py
+++ tektere.py
@@ -3,33 +3,41 @@ import sys,os,ast,curses
 stdscr=curses.initscr()
 height,width=stdscr.getmaxyx()
 class carto:
-  def load(self,dest):
+  def __init__(self): self.fc=0
+  def load(self):
     if 'area' in locals(): del self.area
-    stdscr.clear()
-    with open('data/'+dest) as choice:
+    with open('source') as choice:
       config=ast.literal_eval(choice.readline())
-      script=choice.readlines()
-    self.h=len(script)
-    self.w=len(max(script,key=len))*2
+      self.script=choice.readlines()
+    self.frames=[self.script.index(f) for f in self.script if f.startswith('f')]
+    self.h=len(self.script[self.frames[0]:self.frames[1]])
+    self.w=len(max(self.script,key=len))*2
     self.ypos=(self.h-height)/2
     self.xpos=(self.w-width)/2
     self.area=curses.newpad(self.h+1,self.w+1)
+    self.area.nodelay(1)
     for l in range(1,len(config)):
       r,g,b=config[l]
       curses.init_color(l,r,g,b)
-      curses.init_pair(l,l,l)
-    for n in range(0,len(script)):
+      curses.init_pair(l,0,l)
+  def update(self):
+    self.area.clear()
+    frame=self.script[self.frames[self.fc]+1:self.frames[self.fc+1]]
+    if self.fc<len(self.frames)-2: self.fc+=1
+    else: self.fc=0
+    for i in range(len(frame)):
       x=0
-      for b in list(script[n].rstrip()):
-        if b!=' ': self.area.chgat(n,x,2,curses.color_pair(int(b)))
+      for j in list(frame[i].rstrip()):
+        if j!=' ': self.area.chgat(i,x,2,curses.color_pair(int(j)))
         x+=2
-    stdscr.refresh()
+    self.area.refresh(self.ypos,self.xpos,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2),min(width,(width+self.w)/2))
 m = carto()
 def main(stdscr):
   os.system('setterm -cursor off')
-  m.load('origin')
+  m.load()
   while True:
-    m.area.refresh(m.ypos,m.xpos,max(0,(height-m.h)/2),max(0,(width-m.w)/2),min(height-1,(height+m.h)/2),min(width-1,(width+m.w)/2))
+    stdscr.refresh()
+    m.update()
     k=m.area.getch()
     if k==ord('q'): break
     elif k==ord('s') and m.ypos+height<m.h: m.ypos+=1
