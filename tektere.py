4cc53cf7e60a4cb0ead522cbcd1e9b6d6e7887e9
gwimbrow <graham.wimbrow@gmail.com>
2014-11-11 17:44:54 -0800

    new sources

--- tektere.py
+++ tektere.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import sys,os,ast,curses
+import sys,os,ast,time,random,curses
 stdscr=curses.initscr()
 #stdscr.bkgd(curses.ACS_CKBOARD)
 height,width=stdscr.getmaxyx()
@@ -10,6 +10,7 @@ class carto:
     with open('source') as choice:
       config=ast.literal_eval(choice.readline())
       self.script=choice.readlines()
+    self.rate,self.mod=config[0]
     self.frames=[self.script.index(f) for f in self.script if f.startswith('f')]
     self.h=len(self.script[self.frames[0]+1:self.frames[1]])
     self.w=(len(max(self.script,key=len))-1)*2
@@ -36,6 +37,7 @@ def main(stdscr):
   os.system('setterm -cursor off')
   m.load()
   while True:
+    time.sleep(m.rate*(random.random()/m.mod))
     stdscr.clear()
     m.update()
     k=m.area.getch()
