0d72eef3d46feb2cc52d441b349d2fb1588f7f57
gwimbrow <graham.wimbrow@gmail.com>
2014-11-30 13:51:19 -0800

    30 lines

--- tektere.py
+++ tektere.py
@@ -11,21 +11,18 @@ class carto:
     self.area.timeout(rate)
     self.keyframes=[]
     for f in range(len(self.script)-1):
-      if self.script[f].startswith('#'): self.keyframes.append(f)
+      if self.script[f].startswith('#'): self.keyframes.append(f+1)
   def update(self):
-    y=0
-    for i in range(self.keyframes[self.fc]+1,self.keyframes[self.fc]+self.h+1):
-      self.area.addstr(y,0,self.script[i])
-      y+=1
+    for i in range(self.keyframes[self.fc],self.keyframes[self.fc]+self.h):
+      self.area.addstr(i-self.keyframes[self.fc],0,self.script[i])
     if self.fc<len(self.keyframes)-1: self.fc+=1
     else: self.fc=0
-    self.area.refresh((self.h-height)/2,(self.w-width)/2,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2),min(width,(width+self.w)/2))
+    self.area.refresh((self.h-height)/2,(self.w-width)/2,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2)-1,min(width,(width+self.w)/2)-1)
 m = carto()
 def main(stdscr):
   curses.curs_set(0)
   m.load()
   while True:
-    stdscr.clear()
     m.update()
     k=m.area.getch()
     if k==ord('q'): break
