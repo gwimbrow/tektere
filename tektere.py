ca41d22261aacdd4684843509f29fa201245e86d
gwimbrow <graham.wimbrow@gmail.com>
2014-11-30 13:44:58 -0800

    not sure but ok

--- tektere.py
+++ tektere.py
@@ -1,29 +1,25 @@
 #! /usr/bin/env python
-import curses
+import ast,curses
 stdscr=curses.initscr()
 height,width=stdscr.getmaxyx()
 class carto:
   def __init__(self): self.fc=0
   def load(self):
     with open('source') as choice: self.script=choice.readlines()
-    rate=int(self.script[0])
-    self.frames=[self.script.index(f) for f in self.script if f.startswith('#')]
-    self.h=len(self.script[self.frames[0]+1:self.frames[1]])
-    self.w=len(max(self.script,key=len))-1
-    self.ypos=(self.h-height)/2
-    self.xpos=(self.w-width)/2
+    rate,self.h,self.w=ast.literal_eval(self.script[0])
     self.area=curses.newpad(self.h+1,self.w+1)
     self.area.timeout(rate)
+    self.keyframes=[]
+    for f in range(len(self.script)-1):
+      if self.script[f].startswith('#'): self.keyframes.append(f)
   def update(self):
-   frame=self.script[self.frames[self.fc]+1:self.frames[self.fc+1]]
-   if self.fc<len(self.frames)-2: self.fc+=1
-   else: self.fc=0
-   for y in range(len(frame)):
-     x=0
-     for j in list(frame[y].rstrip()):
-       if j!=' ': self.area.addstr(y,x,j)
-       x+=1
-   self.area.refresh(self.ypos,self.xpos,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2)-1,min(width,(width+self.w)/2)-1)
+    y=0
+    for i in range(self.keyframes[self.fc]+1,self.keyframes[self.fc]+self.h+1):
+      self.area.addstr(y,0,self.script[i])
+      y+=1
+    if self.fc<len(self.keyframes)-1: self.fc+=1
+    else: self.fc=0
+    self.area.refresh((self.h-height)/2,(self.w-width)/2,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2),min(width,(width+self.w)/2))
 m = carto()
 def main(stdscr):
   curses.curs_set(0)
@@ -33,9 +29,5 @@ def main(stdscr):
     m.update()
     k=m.area.getch()
     if k==ord('q'): break
-    elif k==ord('s') and m.ypos+height<m.h: m.ypos+=1
-    elif k==ord('w') and 0<m.ypos: m.ypos-=1
-    elif k==ord('d') and m.xpos+width<m.w: m.xpos+=1
-    elif k==ord('a') and 0<m.xpos: m.xpos-=1
   curses.curs_set(1)
 curses.wrapper(main)
