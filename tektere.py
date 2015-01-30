c2b583f03b1d508fea69dc8572f58fdc505c4c25
gwimbrow <graham.wimbrow@gmail.com>
2015-01-29 19:59:52 -0800

    refreshed

--- tektere.py
+++ tektere.py
@@ -1,36 +1,32 @@
 #! /usr/bin/env python
 import sys,os,ast,curses
-os.system('setfont '+sys.argv[1]+'.psf')
 stdscr=curses.initscr()
 height,width=stdscr.getmaxyx()
 class carto:
-  def __init__(self): self.fc=0
   def load(self):
     with open(sys.argv[1]) as choice:
-      self.config=ast.literal_eval(choice.readline())
+      rate,self.h,self.w,font=ast.literal_eval(choice.readline())
       self.script=choice.readlines()
-    self.rate,self.h,self.w=self.config[0]
-    for c in range(1,3):
-      r,g,b=self.config[c]
-      curses.init_color(c,r,g,b)
-      curses.init_pair(c,c-1,c)
+    os.system('setfont '+font+'.psf')
+    with open(font) as defs:
+      colors=ast.literal_eval(defs.readline()) # a list of two color tuples
+      self.seqs=defs.readlines() # lists of animation sequences
+    for c in range(len(colors)):
+      r,g,b=colors[c]
+      curses.init_color(c+1,r,g,b)
+      curses.init_pair(c+1,c,c+1)
     self.area=curses.newpad(self.h+1,self.w+1)
-    self.area.timeout(self.rate)
-    self.keyframes=[]
-    for f in range(len(self.script)-1):
-      if self.script[f].startswith('#'): self.keyframes.append(f+1)
+    self.area.timeout(rate)
   def update(self):
-    for i in range(self.keyframes[self.fc],self.keyframes[self.fc]+self.h):
-      line=list(self.script[i])
-      for l in range(len(line)-1):
-        for group in [map(str,g) for g in self.config[3:] if line[l] in map(str,g)]:
-          self.area.addstr(i-self.keyframes[self.fc],l,line[l],curses.color_pair(2))
-          if group.index(line[l])<len(group)-1: n=group[group.index(line[l])+1]
-          else: n=group[0]
+    for i in range(1,self.h):
+      line=list(self.script[i].rstrip())
+      for l in range(len(line)):
+        self.area.addstr(i-1,l,line[l],curses.color_pair(2))
+        for cell in [g.rstrip() for g in self.seqs if line[l] in g]:
+          if cell.index(line[l])<len(cell)-1: n=cell[cell.index(line[l])+1]
+          else: n=cell[0]
           line[l]=n
       self.script[i]=''.join(line)
-    if self.fc<len(self.keyframes)-1: self.fc+=1
-    else: self.fc=0
     self.area.refresh((self.h-height)/2,(self.w-width)/2,max(0,(height-self.h)/2),max(0,(width-self.w)/2),min(height,(height+self.h)/2)-1,min(width,(width+self.w)/2)-1)
 m = carto()
 def main(stdscr):
@@ -40,5 +36,6 @@ def main(stdscr):
     m.update()
     k=m.area.getch()
     if k==ord('q'): break
+  # Replace with early function to navigate through files on a cartesian plane. The files themselves are named as coordinate pairs: y.x in this case
   curses.curs_set(1)
 curses.wrapper(main)
